83a1db359dfc7a7d611692b440db7c6a75a7c641
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-05-02 16:52:24 +0300

    task12

--- js/script-12.js
+++ js/script-12.js
@@ -5,29 +5,43 @@ let b = [
   [7, 8, 9],
 ];
 
-console.log(b);
+// console.log(b);
 
 for (let i = 0; i < b.length; i++) {
   // console.log(b[i]);
 
   let c = b[i];
 
+  // console.log(c);
+
   for (let k = 0; k < c.length; k++) {
-    console.log(c[0]);
+    console.log(c[k]);
   }
 }
 
 let d = [1, 0, 0, 0, 0, 0];
 for (let j = 0; j < d.length; j++) {
-  document.querySelector(".out-1").innerHTML = d;
+  document.querySelector(".out").innerHTML = d;
 }
 let j = 0;
-document.querySelector(".btn-1").onclick = () => {
+document.querySelector(".btn").onclick = () => {
   if (j + 1 < 6) {
     d[j] = 0;
     d[j + 1] = 1;
     j = j + 1;
   }
 
-  document.querySelector(".out-1").innerHTML = d;
+  document.querySelector(".out").innerHTML = d;
+};
+
+const btn1 = document.querySelector(".btn-1");
+const out1 = document.querySelector(".out-1");
+let f = [12, 45, 55, 47, 85];
+
+btn1.onclick = function f1() {
+  for (let i = 0; i < f.length; i++) {
+    let g = f[2];
+
+    out1.innerHTML = g;
+  }
 };
